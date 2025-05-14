42ab588415671557f45ec1ebaf54a81b27433792
Nguyen Quy Duc <nqd001205@gmail.com>
2025-05-14 08:52:06 +0700

    -

--- src/main/java/oop/grp1/TestingAlphaVantage.java
+++ src/main/java/oop/grp1/TestingAlphaVantage.java
@@ -1,18 +1,47 @@
 package oop.grp1;
 
-import oop.grp1.Utils.DataFetcher.AlphaVantage;
-import oop.grp1.Utils.JsonBeautifier;
-import oop.grp1.Utils.JsonToSQLite.AlphaVantageJsonToSQLite;
+import java.util.Arrays;
+import java.util.List;
+
+import oop.grp1.Control.DBManager.StockManager;
 
 public class TestingAlphaVantage {
     public static void main(String[] args) {
-        String symbol = "QWE"; 
-        AlphaVantage dataFetcher = new AlphaVantage();
+        List<String> topStocks = Arrays.asList(
+        "AAPL",   // Apple Inc.
+            "MSFT",   // Microsoft Corp.
+            "AMZN",   // Amazon.com Inc.
+            "GOOGL",  // Alphabet Inc. (Class A)
+            "GOOG",   // Alphabet Inc. (Class C)
+            "META",   // Meta Platforms Inc.
+            "TSLA",   // Tesla Inc.
+            "NVDA",   // NVIDIA Corp.
+            "BRK.B",  // Berkshire Hathaway Inc.
+            "UNH",    // UnitedHealth Group
+            "JNJ",    // Johnson & Johnson
+            "V",      // Visa Inc.
+            "PG",     // Procter & Gamble
+            "JPM",    // JPMorgan Chase
+            "HD",     // Home Depot
+            "MA",     // Mastercard Inc.
+            "XOM",    // Exxon Mobil Corp.
+            "PFE",    // Pfizer Inc.
+            "ABBV",   // AbbVie Inc.
+            "KO",     // Coca-Cola Co.
+            "PEP",    // PepsiCo Inc.
+            "MRK",    // Merck & Co.
+            "CVX",    // Chevron Corp.
+            "WMT",    // Walmart Inc.
+            "BAC",    // Bank of America
+            "DIS",    // Walt Disney Co.
+            "INTC",   // Intel Corp.
+            "CSCO",   // Cisco Systems
+            "MCD",    // McDonald's Corp.
+            "ADBE"    // Adobe Inc.
+        );
 
-        String data = JsonBeautifier.beautify(dataFetcher.getData(symbol));
-
-        System.out.println(data);
-
-        AlphaVantageJsonToSQLite.saveJsonToDatabase(data);
+        for (String symbol : topStocks) {
+            StockManager.SymbolToSQLite(symbol);
+        }
     }
 }
