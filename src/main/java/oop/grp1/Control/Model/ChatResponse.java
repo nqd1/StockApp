6ab4b5dd1aa51c2200b2f568dcab826c906f6a48
Hoàng Văn Thắng <Thang.HV235828@sis.hust.edu.vn>
2025-05-23 19:33:44 +0700

    Add Stock Chatbot

---
+++ src/main/java/oop/grp1/Control/Model/ChatResponse.java
@@ -0,0 +1,47 @@
+package oop.grp1.Control.Model;
+
+public class ChatResponse {
+    private long searchTime;
+    private long responseTime;
+    private String responseContent;
+    private String session;
+
+    public ChatResponse(long searchTime, long responseTime, String responseContent, String session) {
+        this.searchTime = searchTime;
+        this.responseTime = responseTime;
+        this.responseContent = responseContent;
+        this.session = session;
+    }
+
+    public long getSearchTime() {
+        return searchTime;
+    }
+
+    public long getResponseTime() {
+        return responseTime;
+    }
+
+    public String getResponseContent() {
+        return responseContent;
+    }
+
+    public String getSession() {
+        return session;
+    }
+
+    public void setSearchTime(long searchTime) {
+        this.searchTime = searchTime;
+    }
+
+    public void setResponseTime(long responseTime) {
+        this.responseTime = responseTime;
+    }
+
+    public void setResponseContent(String responseContent) {
+        this.responseContent = responseContent;
+    }
+
+    public void setSession(String session) {
+        this.session = session;
+    }
+}
